64bec8e9d2fa399fdf51343f434472749ee22daa
Fluturecode <fluturecode@gmail.com>
2023-05-30 18:57:19 -0400

    updatedf assets and added configs

---
+++ scripts/ships.ts
@@ -0,0 +1,66 @@
+import { payer, connection } from "@/lib/vars";
+import { explorerURL, loadPublicKeysFromFile, printConsoleSeparator } from "@/lib/helpers";
+
+import { PublicKey } from "@solana/web3.js";
+import { Metaplex, bundlrStorage, keypairIdentity } from "@metaplex-foundation/js";
+
+(async () => {
+
+  console.log("Payer address:", payer.publicKey.toBase58());
+
+  // load the stored PublicKeys for ease of use
+  let localKeys = loadPublicKeysFromFile();
+
+  // ensure the desired script was already run
+  if (!localKeys?.tokenMint)
+    return console.warn("No local keys were found. Please run '3.createTokenWithMetadata.ts'");
+
+  const tokenMint: PublicKey = localKeys.tokenMint;
+
+  console.log("==== Local PublicKeys loaded ====");
+  console.log("Token's mint address:", tokenMint.toBase58());
+  console.log(explorerURL({ address: tokenMint.toBase58() }));
+  
+  /**
+   * define our ship's JSON metadata
+   * checkout: https://nft.storage/ to help store images
+   */
+  const metadata = {
+    name: "",
+    image: "",
+  };
+
+  // create an instance of Metaplex sdk for use
+  const metaplex = Metaplex.make(connection)
+    // set our keypair to use, and pay for the transaction
+    .use(keypairIdentity(payer))
+    // define a storage mechanism to upload with
+    .use(bundlrStorage());
+
+  // upload the JSON metadata
+
+  const { uri } = await metaplex.nfts().uploadMetadata(metadata);
+
+  console.log(uri);
+
+  // create a new nft using the metaplex sdk
+  const { nft, response } = await metaplex.nfts().create({
+    uri,
+    name: metadata.name,
+
+    // `sellerFeeBasisPoints` is the royalty that you can define on nft
+    sellerFeeBasisPoints: 500, // Represents 5.00%.
+
+    //
+    isMutable: true,
+  });
+
+  console.log(nft);
+
+  printConsoleSeparator("NFT created:");
+  console.log(explorerURL({ txSignature: response.signature }));
+
+  return;
+
+
+})();
